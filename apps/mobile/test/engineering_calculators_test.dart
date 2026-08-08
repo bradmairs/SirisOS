7b58e5deb65b56c45a0a1a8e58fe8e81cfa02165
bradmairs <brad6491@gmail.com>
2026-08-08 13:07:38 +1000

    Fix engineering test package import

--- apps/mobile/test/engineering_calculators_test.dart
+++ apps/mobile/test/engineering_calculators_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:sirisos/src/core/engineering_calculators.dart';
+import 'package:siris_os/src/core/engineering_calculators.dart';
 
 void main() {
   group('EngineeringCalculators', () {
