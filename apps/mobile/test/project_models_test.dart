b4104427333fba2740fce255c69353b7c284af5e
bradmairs <brad6491@gmail.com>
2026-08-09 09:06:31 +1000

    Fix SirisOS package import in project tests

--- apps/mobile/test/project_models_test.dart
+++ apps/mobile/test/project_models_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:sirisos/src/services/project_service.dart';
+import 'package:siris_os/src/services/project_service.dart';
 
 void main() {
   test('ProjectRecord parses project identity and lifecycle', () {
